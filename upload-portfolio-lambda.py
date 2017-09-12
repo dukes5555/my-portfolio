712aca2f3897a9243314f5c8d0616a385a24ae35
Nick Caccese <n.caccese@gmail.com>
2017-09-12 18:01:42 -0400

    Update portfolio deployment function for lambda to send notification via SNS

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,22 +5,35 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:414370728305:deployPortfolioTopic')
     
-    portfolio_bucket = s3.Bucket('portfolio.nickcaccese.com')
+    try:
     
-    build_bucket = s3.Bucket('portfoliobuild.nickcaccese.com')
-    build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('portfolio.nickcaccese.com')
+        
+        build_bucket = s3.Bucket('portfoliobuild.nickcaccese.com')
+        build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        print "Job done!"
+        
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+        
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed succesfully.")
+        raise
     
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
     
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-            
-    print "Job done!"
     return 'Hello from Lambda'
